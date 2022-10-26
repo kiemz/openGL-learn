deefb4e35306566a17abca6efb3cd3f86aa0f990
yipin <yipin@raysengine.com>
2022-10-26 18:39:41 +0800

    暂存

---
+++ OpenGL/src/Render.cpp
@@ -0,0 +1,16 @@
+#include "Render.h"
+#include <iostream>
+
+void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+bool GLLogCall()
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << ")" << std::endl;
+        return false;
+    }
+    return true;
+}
---
+++ OpenGL/src/Render.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <GL/glew.h>
+
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall())
+
+void GLClearError();
+bool GLLogCall();
