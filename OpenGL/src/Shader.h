05a4753b36525a3bc59cc2028836bf6b55fd9309
kiemz <511585052@qq.com>
2022-10-27 23:48:50 +0800

    texture

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -28,5 +28,7 @@ public:
 	void Bind() const;
 	void Unbind() const;
 
-	void SerUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform1f(const std::string& name, float value);
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 };
--- OpenGL/src/Texture.cpp
+++ OpenGL/src/Texture.cpp
@@ -12,8 +12,8 @@ Texture::Texture(const std::string& path)
 	glGenTextures(1, &m_RenderID);
 	glBindTexture(GL_TEXTURE_2D, m_RenderID);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
